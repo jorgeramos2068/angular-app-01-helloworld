a82ffb127f7cd7ccfc98e9408118742d5065ee15
Jorge Ramos <jorgeramos2068@gmail.com>
2021-01-19 21:01:57 -0700

    Initial commit

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component( {
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+} )
+export class BodyComponent
+{
+  frase:any = {
+    mensaje: 'Un gran poder requiere una gran responsabilidad',
+    autor: 'Ben Parker'
+  }
+
+  banderaFrase:boolean = true;
+
+  personajes:string[] = [
+    'Spiderman',
+    'Venom',
+    'Carnage',
+    'Dr. Strange'
+  ];
+}
